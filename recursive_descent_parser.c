2a752dda0c2a1bfdaebf3e256312c55b25559c08
tsn67 <tomsebastian67@gmail.com>
2025-10-26 11:18:26 +0000

    lex program for regular expression

--- recursive_descent_parser.c
+++ recursive_descent_parser.c
@@ -33,7 +33,7 @@ int EP(char** input) {
             if (EP(input))
                 return 1;
     }
-    *input = temp;
+    *input = temp; //reset the input if +TE' was not success
     return 1;
 }
 
