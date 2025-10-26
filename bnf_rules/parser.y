5858db7a42ff7eca5fd37ac98871ed23a64314e9
tsn67 <tomsebastian67@gmail.com>
2025-10-26 09:52:21 +0000

    removed wrong recursive parser

--- bnf_rules/parser.y
+++ bnf_rules/parser.y
@@ -1,7 +1,6 @@
 %{
     #include <stdio.h>
     
-    
     extern FILE* yyin;
 %}
 
--- recursive_descent_parser.c
+++ recursive_descent_parser.c
@@ -1,127 +1,92 @@
-/* 
-	E -> TE'
-	E' -> +TE' | #
-	T -> FT'
-	T' -> *FT' | #
-	F -> (E) | NUM
-*/
-
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int T();
-int E();
-int Z();
-int Y();
-int F();
+/*
 
+E → T E'
+E' → + T E' | ε
+T → F T'
+T' → * F T' | ε
+F → (E) | d
 
-char* input;
+*/
 
-//E' - Z
-//T' - Y
+int E(char**);
+int EP(char**);
+int T(char**);
+int TP(char**);
+int F(char**);
 
-int E() {
-	int val = T();
-	if (val == 1) {
-		val = Z();
-	} else return 0;
-	
-	if (val == 1) 
-		return 1;
-	return 0;
+int E(char** input) {
+    if (T(input))
+        if(EP(input))
+            return 1;
+    return 0;
 }
 
-
-int Z() {
-	if (*input == '+') {
-		input++;
-		int val = T();
-		
-		if (val == 1) {
-			val = Z();
-		} else return 0;
-		
-		if (val == 1) return 1;
-		return 0;
-	} 
-	
-	return 1;
+int EP(char** input) {
+    char* temp = *input;
+    if (**input == '+') {
+        (*input)++;
+        if (T(input))
+            if (EP(input))
+                return 1;
+    }
+    *input = temp;
+    return 1;
 }
 
-int T() {
-	int val = F();
-	
-	if (val == 1) {
-		val = Y();
-		
-		if (val == 1)
-			return 1;
-	}
-	
-	return 0;
-
+int T(char** input) {
+    if (F(input))
+        if (TP(input))
+            return 1;
+    return 0;
 }
 
-int F() {
-	if (*input == '(') {
-		input++;
-		int val = E();
-		if (val == 1) {
-			if (*input == ')') {
-				input++;
-				return 1;
-			} else return 0;
-		} 
-		return 0;
-	} else {
-		//printf("Hello %s", input);
-		char c = *input;
-		if ('0' <= c && c <= '9') {
-			input++;
-			return 1;
-		}
-		//return 0;
-	
-	}
-	
-	return 0;
-	
+int TP(char** input) {
+    char* temp = *input;
+    if (**input == '*') {
+        (*input)++;
+        if (F(input))
+            if (TP(input))
+                return 1;
+    }
+    *input = temp;
+    return 1;
 }
 
-
-int Y() {
-	if (*input == '*') {
-		input++;
-		int val = F();
-		
-		if (val == 1) {
-			val = Y();
-			if (val == 1)
-				return 1;
-			else return 0;
-		} else return 0;
-	} 
-	return 1;
+int F(char** input) {
+    char* temp = *input;
+    if (**input == 'd') {
+        (*input)++;
+        return 1;
+    } else if (**input == '(') {
+        (*input)++;
+        if (E(input)) {
+            if (**input == ')') {
+                (*input)++;
+                return 1;
+            }
+        }
+    }
+    *input = temp;
+    return 0;
 }
+
 
 int main() {
-	
-	char temp[100];
-	
-	printf("Enter the input: ");
-	scanf("%s", temp);
-	
-	input = temp;
-	int isValid = E();
-	if (isValid == 1) {
-		if (*input != '\0') {
-			printf("Not Valid\n");
-			return 0;
-		}
-		printf("Valid!\n");
-	} else {
-		printf("Not Valid!\n");
-	}
+    while (1) {
+        printf("Enter the input: ");
+        char temp1[100];
+        scanf("%s", temp1);
 
+        char* temp2 = temp1;
+        char** input;
+        input = &temp2;
+
+        if (E(input) && **input == '\0')
+            printf("Successful!\n");
+        else printf("Invalid\n");
+    }
 }
